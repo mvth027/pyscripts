891c426b929645067bbb174f9717eb55e89d6867
Yaroslav Pershin <31351445+meth027@users.noreply.github.com>
2018-10-01 10:53:10 +0300

    Update regEx1.py

--- regEx1.py
+++ regEx1.py
@@ -1,36 +1,74 @@
 import re
 
 dhcpPool = '''
-!
-ip dhcp pool POOL0
- network 172.16.0.0/24
- domain-name cisco.com
- dns-server 172.16.1.102 172.16.2.102
- netbios-name-server 172.16.1.103 172.16.2.103 
- netbios-node-type h-node
-!
-ip dhcp pool POOL1
- network 172.16.1.0/24
- default-router 172.16.1.100 172.16.1.101 
- lease 30 
-!
-ip dhcp pool POOL2
- network 172.16.2.0/24
- default-router 172.16.2.100 172.16.2.101 
- lease 30
+
+ip dhcp pool HEX_WIFI.KIROV.KB.HLYNOV.Urickogo40
+ vrf HEX-WIFI
+ network 100.68.3.0 255.255.255.224
+ dns-server 213.87.2.88 213.87.2.89 
+ default-router 100.68.3.1 
+ lease 0 0 8
+ip dhcp pool HEX_WIFI.KIROV.KB.HLYNOV.Vorovskogo135
+ vrf HEX-WIFI
+ network 100.68.2.192 255.255.255.224
+ dns-server 213.87.2.88 213.87.2.89 
+ default-router 100.68.2.193 
+ lease 0 0 8
+ip dhcp pool HEX_WIFI.KIROV.KB.HLYNOV.Oktyabskiy155
+ vrf HEX-WIFI
+ network 100.68.3.64 255.255.255.224
+ dns-server 213.87.2.88 213.87.2.89 
+ default-router 100.68.3.65 
+ lease 0 0 8
+
 '''
 
-regex = ('ip dhcp pool (\w+)')
-regexX = ('network \w+.\w+.\w+.\w+/\w+')
+interfaces = '''
+Port-channel1.15201401 100.68.3.129    YES manual up                    up      
+Port-channel1.15201409 100.68.3.33     YES manual up                    up      
+Port-channel1.15201413 100.68.3.225    YES manual up                    up      
+Port-channel1.15201447 100.68.3.161    YES manual up                    up      
+Port-channel1.15201456 100.68.3.193    YES manual up                    up      
+Port-channel1.15201701 100.68.3.97     YES manual up                    up      
+Port-channel1.15203454 100.68.3.1      YES manual up                    up      
+Port-channel1.15203496 100.68.3.65     YES manual up                    up
+Port-channel1.15201465 100.68.2.97     YES NVRAM  up                    up      
+Port-channel1.15201493 100.68.2.65     YES manual up                    up      
+Port-channel1.15201704 100.68.2.1      YES NVRAM  up                    up      
+Port-channel1.15201712 100.68.2.225    YES manual up                    up      
+Port-channel1.15201716 100.68.2.129    YES NVRAM  up                    up      
+Port-channel1.15203495 100.68.2.193    YES manual up                    up
 
-m = re.findall(regex, dhcpPool)
-n = re.findall(regexX, dhcpPool)
+'''
 
-x = dict(zip(m,n))
 
-for key, value in x.items():
-        print(key, value)
 
+
+poolNameFind = ('ip dhcp pool (\S+)')
+networkFind = ('network \w+.\w+.\w+.\w+ \w+.\w+.\w+.\w+')
+defaultRouterFind = ('default-router \w+.\w+.\w+.\w+')
+
+
+poolName = re.findall(poolNameFind, dhcpPool)
+network = re.findall(networkFind, dhcpPool)
+defaultRouter = re.findall(defaultRouterFind, dhcpPool)
+
+output = dict(zip(poolName,network))
+
+#for key, value in output.items():
+#        print(key, value)
+        
+
+try:   
+ i = 0
+ while i <= len(poolName):
+     print (str(poolName[i]))
+     print (str(network[i]))
+     print (str(defaultRouter[i]))
+     print ("")
+     i += 1
+except IndexError:
+    print ('')
 
 
 
